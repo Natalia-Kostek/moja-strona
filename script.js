bfc28f95da7c28836774dac92873e71d047ebed5
Natalia-Kostek <kosteknatalia83@gmail.com>
2026-06-28 17:32:20 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,8 @@
-window.addEventListener("load", () => {
+const slides = document.querySelectorAll(".slide");
+let index = 0;
 
-  const slides = document.querySelectorAll(".slide");
-
-  if (!slides.length) return;
-
-  let index = 0;
-
-  function show(i){
-    slides.forEach(s => s.classList.remove("active"));
-    slides[i].classList.add("active");
-  }
-
-  show(0);
-
-  setInterval(() => {
-    index = (index + 1) % slides.length;
-    show(index);
-  }, 4000);
-
-});
+setInterval(() => {
+  slides.forEach(s => s.classList.remove("active"));
+  index = (index + 1) % slides.length;
+  slides[index].classList.add("active");
+}, 4000);
