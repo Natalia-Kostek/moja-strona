229d69708dbcbf735cc0548d6a9f668e7b64cb08
Natalia-Kostek <kosteknatalia83@gmail.com>
2026-06-28 17:19:19 +0200

    Update script.js

--- script.js
+++ script.js
@@ -47,3 +47,20 @@ window.addEventListener("load", () => {
   }, 4000);
 
 });
+window.addEventListener("load", () => {
+
+  const sections = document.querySelectorAll(".section");
+
+  const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      if(entry.isIntersecting){
+        entry.target.classList.add("show");
+      }
+    });
+  }, {
+    threshold: 0.1
+  });
+
+  sections.forEach(sec => observer.observe(sec));
+
+});
